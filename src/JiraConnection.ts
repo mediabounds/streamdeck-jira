4053d356868a8db8ea85a550704c5d864132ecca
Daniel Pfeiffer <daniel@mediabounds.com>
2023-02-01 21:10:07 -0600

    Adds support for personal access tokens (#1)

---
+++ src/JiraConnection.ts
@@ -0,0 +1,34 @@
+import Client, { Authenticator, BasicAuth, TokenAuth } from "./Client";
+import { DefaultPluginSettings } from "./JiraPluginSettings";
+
+/**
+ * Convenience class for getting connected to Jira.
+ */
+export class JiraConnection {
+  /**
+   * Creates a new Client for making API requests.
+   * 
+   * @param settings - Connection settings for the client.
+   * @returns A configured Client.
+   */
+  public static getClient(settings: DefaultPluginSettings): Client {
+    const {domain, email: username, token: key, strategy} = settings;
+
+    if (!domain) {
+      throw new Error('A domain must be set');
+    }
+
+    if (!key) {
+      throw new Error('An API token must be set');
+    }
+
+    let authenticator: Authenticator;
+    if (strategy === 'PAT') {
+      authenticator = new TokenAuth(key);
+    } else {
+      authenticator = new BasicAuth(username, key);
+    }
+
+    return new Client(`https://${domain}`, authenticator);
+  }
+}
